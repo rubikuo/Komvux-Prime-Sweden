39c26dea8da2fd3c164cb36c7645a898e7898f06
Rubi Kuo <rubitiara@gmail.com>
2019-03-16 23:57:43 +0100

    added sort & search and clear

--- app.js
+++ app.js
@@ -1,22 +1,96 @@
 $(() => {
 
     const products = [
-        { name: 'SALIS', price: 1500, id: 1, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'http://www.rediscoverycentre.ie/wp-content/uploads/2017/03/dsc3590adone-1-300x300.jpg', category: 'Free style' },
-        { name: 'CATIY', price: 2000, id: 2, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'http://myparisian-style.com/wp-content/uploads/2018/11/fashion-cheap-sweater-dresses-2018-long-sleeve.jpg_350x350-300x300.jpg', category: 'Free style' },
-        { name: 'DREIE', price: 2250, id: 3, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://imgs.sundaymore.com/wp-content/uploads/2016/04/771e33dbead40d51eb17824a4e8cc32e-300x300.jpg', category: 'Free style' },
-        { name: 'TINVI', price: 2300, id: 4, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'http://portlandinterviewmagazine.com/wp-content/uploads/2017/02/HAL_4800-300x300.jpg', category: 'Party Time' },
-        { name: 'DESIL', price: 2890, id: 5, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://iconicimages.net/app/uploads/2017/01/NP_PE_AH004-300x300.jpg', category: 'Party Time' },
-        { name: 'JAMIQ', price: 2050, id: 6, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlBYvuzC90-1m1woLiqDDt8CnlJUleQ9ihf96WnhPeDFbfFvZxbg', category: 'Party Time' },
-        { name: 'QUEIN', price: 1230, id: 7, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://thestripe.com/wp-content/uploads/2018/10/a-cozy-fall-outfit-featured-300x300.jpg', category: 'Stay Cozy' },
-        { name: 'PHIBI', price: 2790, id: 8, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeCgUzeWidGqx3ycwGz-OUMXoOv3OTJUqM8O1DzXzYPYX4Jfb2gA', category: 'Stay Cozy' },
-        { name: 'BELLE', price: 1520, id: 9, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://krochetkids-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/Genevieve-Davis_Known-Supply-Fall-2019-326-of-447-300x300.jpg', category: 'Stay Cozy' },
-        { name: 'HIBIS', price: 1230, id: 10, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://m.media-amazon.com/images/S/aplus-seller-content-images-us-east-1/ATVPDKIKX0DER/A3GZDTM3QP2SDS/e56ba951-d4b9-4062-a450-56746bda0deb._CR0,0,300,300_PT0_SX300__.jpg', category: 'Beach Fun' },
-        { name: 'LISAI', price: 1890, id: 11, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://m.media-amazon.com/images/S/aplus-seller-content-images-us-east-1/ATVPDKIKX0DER/A3GZDTM3QP2SDS/66fa5c66-b12c-4d0b-93ab-8e1333e13423._CR0,0,1000,1000_PT0_SX300__.jpg', category: 'Beach Fun' },
-        { name: 'RUBIS', price: 1330, id: 12, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCoYIGyWqBJllIV-KdWzMSFZRJW2CfI9p65ySFpoP7yw-jaaAixQ', category: 'Beach fun' },
-        { name: 'OLIVI', price: 1900, id: 13, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'http://gaitaobao2.alicdn.com/tfscom/i1/2842781794/O1CN01SVJQXR1P7h1eq7zN3_!!2842781794.jpg_300x300.jpg', category: 'Winter Mys' },
-        { name: 'PHIBI', price: 2380, id: 14, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://i2.wp.com/www.girlscosmo.com/wp-content/uploads/2012/05/Maxi-Dress.jpg', category: 'Winter Mys' },
-        { name: 'ALISA', price: 1120, id: 15, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://i.ebayimg.com/images/g/Q8MAAOSwDFBaBUiL/s-l300.jpg', category: 'Winter Mys' }
+        { name: 'SALIS', price: 1500, id: 1, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/1.jpg', category: 'Free style' },
+        { name: 'CATIY', price: 2000, id: 2, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/2.jpg', category: 'Free style' },
+        { name: 'DREIE', price: 2250, id: 3, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/3.jpg', category: 'Free style' },
+        { name: 'TINVI', price: 2300, id: 4, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/4.jpg', category: 'Party Time' },
+        { name: 'DESIL', price: 2890, id: 5, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/5.jpg', category: 'Party Time' },
+        { name: 'JAMIQ', price: 2050, id: 6, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/6.jpeg', category: 'Party Time' },
+        { name: 'QUEIN', price: 1230, id: 7, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/7.jpg', category: 'Stay Cozy' },
+        { name: 'PHIBI', price: 2790, id: 8, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/8.jpg', category: 'Stay Cozy' },
+        { name: 'BELLE', price: 1520, id: 9, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/9.jpg', category: 'Stay Cozy' },
+        { name: 'HIBIS', price: 1230, id: 10, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/10.jpg', category: 'Beach Fun' },
+        { name: 'LISAI', price: 1890, id: 11, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/11.jpg', category: 'Beach Fun' },
+        { name: 'RUBIS', price: 1330, id: 12, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/12.jpg', category: 'Beach fun' },
+        { name: 'OLIVI', price: 1900, id: 13, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/13.jpg', category: 'Winter Mys' },
+        { name: 'PHIBI', price: 2380, id: 14, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/14.jpg', category: 'Winter Mys' },
+        { name: 'ALISA', price: 1120, id: 15, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/15.jpg', category: 'Winter Mys' }
     ];
+
+  
+
+    //-------------------to sort by price with only one button -----------//
+    //    let counter = 0;
+    //     // to connect sort button with js
+    //     let sortButton = document.querySelector(".sortbutton");
+    //     // to create click funtion, set button original value 0, after click becomes 1 
+
+    //     sortButton.addEventListener("click",function(){
+    //     if (counter === 0){
+    //         counter = 1;
+    //     let sortArray = products.sort(function(a,b){
+    //         if(a.price >b.price){
+    //             return 1;
+    //         }
+    //         else {return -1};
+
+    //     })
+
+    // appendList(sortArray);
+    // }
+    // else if (counter === 1){
+    //     console.log(counter);
+    //     counter = 0;
+    // let sortArray = products.sort(function(a,b){
+    //     if(a.price >b.price){
+    //         return -1;
+    //     }
+    //     else {return 1};
+
+    // })
+    // appendList(sortArray);
+    // }
+
+    // }) 
+
+
+    // --------------------to sort price from low to high or high to low from top navigation -----------------//
+
+
+
+    let sortPriceFromLow = document.querySelector("#sort-price-low-high");
+    sortPriceFromLow.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        let sortArray = products.sort((a, b) => a.price > b.price ? 1 : -1);
+        appendList(sortArray);
+    });
+
+    let sortPriceFromHigh = document.querySelector("#sort-price-high-low");
+    sortPriceFromHigh.addEventListener("click", (e) => {
+        e.preventDefault();
+        let sortArray = products.sort((a, b) => a.price < b.price ? 1 : -1);
+        appendList(sortArray);
+    });
+
+    // ------------------- search product ---------------------------//
+    /* although it can search out the things but seached out product  can not be added into cart.. */
+
+    let searchField = document.querySelector(".searchfield");
+    searchField.addEventListener("keyup", function (e) {
+        // console.log (e.target.value);
+        let myTarget = e.target.value;
+        let filterList = products.filter(
+            (product) => {
+                return product.name.toLowerCase().indexOf(myTarget.toLowerCase()) !== -1
+
+            }
+        )
+        appendList(filterList);
+    })
+
+    // ----------------- to show all products on the page -------------------//
 
     const appendList = (array) => {
         const template = array.map((item, id) => {
@@ -32,11 +106,13 @@ $(() => {
             </div>
 
                     `;
-
         });
         $('.product-list').html(template);
     }
     appendList(products);
+
+
+   //--------------------Cart & add to cart ---------------------// 
     let cart = [];
 
     const addToCart = (array, id) => {
@@ -56,13 +132,11 @@ $(() => {
 
         $('span.amount').text(cart.length);
         $('.cart-list').append(item);
-    
-}
 
-    
+    }
 
 
-
+//----------------remove from cart one by one ------------------------//
     const removeFromCart = (array, removedItem) => {
         array.splice(removedItem, 1);
 
@@ -79,40 +153,62 @@ $(() => {
 
         $('span.amount').text(array.length);
     }
-    
-    // To calculateTotal Price
-    const totalAmount =() => {
+
+    //-------------------- To calculateTotal Price -----------------//
+    const totalAmount = () => {
         let total = 0;
-         cart.forEach(function(a){ 
-          total += a.price; 
+        cart.forEach(function (a) {
+            total += a.price;
 
         })
         return total;
-     } 
-    
+    }
+
+    //I wonder what this part is about ? what is (.product)? did I link to wrong place?
+
     $('.product').on('click', 'button', (e) => {
         let id = e.currentTarget.id;
         // +id : + sign makes it a number
-        addToCart(products, +id); 
+        addToCart(products, +id); // call the addToCart Function with arrguments products array and button id( which is set quals to product id)
 
         $(".totalAmount").text(totalAmount);
-       
-       
-      
+
     });
 
     $('.cart-list').on('click', 'button', (e) => {
         let item = $(e.currentTarget).closest('li').remove();
         removeFromCart(cart, item);
         populateCart(cart);
+        
 
         $(".totalAmount").text(totalAmount);
-        
+
     });
-    
-    document.getElementById('dropdownitem').addEventListener('click', function (event) {
-        event.stopPropagation();
-      });
+
+
+  //---------------------------- Clear All -------------------------//
+
+  /* It sucessfully clear all the items and amount in the cart but 
+  when I re-select the products, item will not show in the cart, only total amount shows up */
+  
+    const removeAll = () => {
+        $('.cart-list').remove();
+        cart =[];
+
+        }; 
+   
+    $('.clear-all').click((e) => {
+        removeAll();
+        $('span.amount').text(0);
+        $(".totalAmount").text(totalAmount);
+
+    });
+
+  
+
+    // document.getElementById('dropdownitem').addEventListener('click', function (event) {
+    //     event.stopPropagation();
+    //   });
 
 
 
