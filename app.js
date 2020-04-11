a4057a3c96edab4c88e6ffd5845757edb8ee3050
Rubi Kuo <rubitiara@gmail.com>
2020-04-11 16:19:26 +0200

    update photo

--- app.js
+++ app.js
@@ -18,102 +18,124 @@ $(() => {
         { name: 'ALISA', price: 1120, id: 15, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'https://i.ebayimg.com/images/g/Q8MAAOSwDFBaBUiL/s-l300.jpg', category: 'Winter Mys' }
     ];
 
-    const appendList = (array) => {
-        const template = array.map((item, id) => {
-            return `
-            
-            
-            <div class="card product d-inline-flex mt-5 mb-2 mr-3 ml-3" style="width: 15rem;" id="${item.id}">
-            <img src="${item.picture}" class="card-img-top" alt="${item.description}">
-            <div class="card-body mr-auto ml-auto">
-            <h5 class="card-title">${item.name} - <span class="category">${item.category}</span> <small>${item.price}kr</small></h5>
-            <p class="card-text">${item.description}</p>
-            <button type="button" id="${item.id}">Buy Now!</button>
+   
+let cart = [];
+
+const appendList = (array) => {
+    const template = array.map((item, id) => {
+        return `
+        <div class="card product d-inline-flex mt-5 mb-2 mr-3 ml-3" style="width: 15rem;" id="${item.id}">
+        <img src="${item.picture}" class="card-img-top" alt="${item.description}">
+        <div class="card-body mr-auto ml-auto">
+        <h5 class="card-title">${item.name} - <span class="category">${item.category}</span> <small>${item.price}kr</small></h5>
+        <p class="card-text">${item.description}</p>
+        <button type="button" id="${item.id}">Buy Now!</button>
+        </div>`;
+    });
+    $('.product-list').html(template);
+};
+
+const addToCart = (array, id) => {
+    let a = array.find(i => i.id === id); //after clicking the buying button, if the button's id equals item's id, push the item a into the cart
+
+    cart.push(a);
+
+    const item = `
+            <li class="item pl-3 pb-1" id="${a.id}">
+            <div class="d-flex">
+            <span class="pb-1 cartitem"><i class="fas fa-heart"></i> ${a.name}</span> <p class="ml-5 pl-5"> ${a.price} kr</p>
+            <button type="button" class="btn text-dark d-flex mt-0 pt-0 mr-0 pr-0"><i class="fas fa-times"></i></button>
             </div>
+            </li>
+            `;
+    $('span.amount').text(cart.length);
+    $('.cart-list').append(item);
+};
 
-                    `;
+//----------------remove from cart one by one ------------------------//
+const removeFromCart = (array, removedItem) => {
+    array.splice(removedItem, 1);
+};
 
-        });
-        $('.product-list').html(template);
-    }
-    appendList(products);
-    let cart = [];
+const populateCart = (array) => {
+    let item = `
+            <li class"item" id="${array.id}">
+            <span class="cartitem">${array.name}</span>
+            <button type="button" class="cancelbtn"><i class="fas fa-times"></i></button>
+            </li>
+            `;
+    $('span.amount').text(array.length);
+};
 
-    const addToCart = (array, id) => {
-        let a = array.find(i => i.id === id); //after clicking the buying button, if the button's id equals item's id, push the item a into the cart
+//-------------------- To calculateTotal Price -----------------//
+const totalAmount = () => {
+    let total = 0;
+    cart.forEach(a => { total += a.price; });
+    return total;
+};
 
-        cart.push(a);
+//-------------------- To Remowe All --------------------------//
+const removeAll = () => {
+    let listArray = $('.cart-list')[0].children;
+    for (let i = 0; i < listArray.length; i++) {
+        listArray[i].remove();
+        i--;
+    };
+    cart = [];
+};
 
-
-        const item = `
-                <li class="item pl-3 pb-1" id="${a.id}">
-                <div class="d-flex">
-                <span class="pb-1 cartitem"><i class="fas fa-heart"></i> ${a.name}</span> <p class="ml-5 pl-5"> ${a.price} kr</p>
-                <button type="button" class="btn text-dark d-flex mt-0 pt-0 mr-0 pr-0"><i class="fas fa-times"></i></button>
-                </div>
-                </li>
-                `;
-
-        $('span.amount').text(cart.length);
-        $('.cart-list').append(item);
-    
-}
-
-    
-
-
-
-    const removeFromCart = (array, removedItem) => {
-        array.splice(removedItem, 1);
-
-    }
-
-    const populateCart = (array) => {
-        let item = `
-             
-                <li class"item" id="${array.id}">
-                <span class="cartitem">${array.name}</span>
-                <button type="button" class="cancelbtn"><i class="fas fa-times"></i></button>
-                </li>
-                `;
-
-        $('span.amount').text(array.length);
-    }
-    
-    // To calculateTotal Price
-    const totalAmount =() => {
-        let total = 0;
-         cart.forEach(function(a){ 
-          total += a.price; 
-
-        })
-        return total;
-     } 
-    
-    $('.product').on('click', 'button', (e) => {
-        let id = e.currentTarget.id;
-        // +id : + sign makes it a number
-        addToCart(products, +id); 
-
-        $(".totalAmount").text(totalAmount);
-       
-       
-      
+$(() => {
+    $('#sort-price-low-high').click(e => {
+        e.preventDefault();
+        let sortArray = products.sort((a, b) => a.price > b.price ? 1 : -1);
+        appendList(sortArray);
     });
 
+    $('#sort-price-high-low').click(e => {
+        e.preventDefault();
+        let sortArray = products.sort((a, b) => a.price < b.price ? 1 : -1);
+        appendList(sortArray);
+    });
+
+    // ------------------- search product ---------------------------//
+    /* although it can search out the things but seached out product  can not be added into cart.. */
+    let searchField = document.querySelector(".searchfield");
+    searchField.addEventListener("keyup", function (e) {
+        let myTarget = e.target.value;
+        let filterList = products.filter(product => {
+                return product.name.toLowerCase().indexOf(myTarget.toLowerCase()) !== -1; 
+            });
+        appendList(filterList);
+    });
+
+    // ----------------- to show all products on the page -------------------//
+    appendList(products);
+    //--------------------Cart & add to cart ---------------------// 
+
+    // item removed function
     $('.cart-list').on('click', 'button', (e) => {
+        e.preventDefault();
         let item = $(e.currentTarget).closest('li').remove();
         removeFromCart(cart, item);
         populateCart(cart);
 
         $(".totalAmount").text(totalAmount);
-        
     });
-    
-    document.getElementById('dropdownitem').addEventListener('click', function (event) {
-        event.stopPropagation();
-      });
 
-
+    //---------------------------- Clear All -------------------------//
+    $('.clear-all').click((e) => {
+        e.preventDefault();
+        removeAll();
+        $('span.amount').text(0);
+        $(".totalAmount").text(totalAmount); //because cart has nothing inside so total amount will be 0
+    });
+            
+    $('.product-list').on('click', 'button', (e) => {
+        e.preventDefault();
+        let id = e.currentTarget.id;
+        addToCart(products, +id); 
+        $(".totalAmount").text(totalAmount);
+    });
+}); 
 
 }); 
